78b833614dcb2eafa0827230699866810368279a
NO ONE <38581702+HACKER097@users.noreply.github.com>
2021-10-26 23:15:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import praw
 import os
 import threading 
 
+import hashlib
+
 from keep_alive import keep_alive
 
 keep_alive()
@@ -12,46 +14,58 @@ r = praw.Reddit(client_id=os.getenv("client_id"), client_secret=os.getenv("clien
 subreddit = r.subreddit("AnonReddit")
 
 def comment_stream(r, subreddit):
-    for comment in subreddit.stream.comments():
+    for comment in subreddit.stream.comments(skip_existing=True):
         if not str(comment.author) == "Anon-Bot":
             body = comment.body
             parent = r.comment(comment.parent_id)
             comment.mod.remove()
-           
-        
-            body = body.split(" ")
 
-            for i in range(len(body)):
-                if body[i] == "!pass":
-                    user_hash = zlib.crc32(bytes(body[i+1] + str(comment.author) , "utf-8" ))
+            if "!pass " in body:
+                body = body.split(" ")
 
-                    body.pop(i)
-                    body.pop(i+1)
+                for i in range(len(body)):
+
+                    if body[i] == "!pass":
+                        user_hash = hashlib.sha256(bytes(body[i+1] + str(comment.author) , "utf-8" )).hexdigest()
+
+                        body.pop(i)
+                        body.pop(i)
 
 
-                    body = "USERHASH:" + str(user_hash) + "\n\n" + " ".join(body)
-                    body = body[:10000]
+                        body = "*USERHASH: " + str(user_hash) + "*" + " \n\n " + " ".join(body)
+                        body = body[:10000]
 
-                    break
-                
-                parent.reply(body)
-                break
-         
-             
-                    
-            body = " ".join(body)
-     
+                        break
+
             parent.reply(body)
 
 def post_stream(r, subreddit):
-    for post in subreddit.stream.submissions():
+    for post in subreddit.stream.submissions(skip_existing=True):
         if not str(post.author) == "Anon-Bot":
             title = post.title
             body = post.selftext
 
             post.mod.remove()
 
+            if "!pass " in body:
+
+                body = body.split(" ")
+
+                for i in range(len(body)):
+                    if body[i] == "!pass":
+                        user_hash = hashlib.sha256(bytes(body[i+1] + str(post.author) , "utf-8" )).hexdigest()
+
+                        body.pop(i)
+                        body.pop(i)
+
+
+                        body = "*USERHASH: " + str(user_hash) + "*" + " \n\n " + " ".join(body)
+                        body = body[:10000]
+
+                        break
+
             subreddit.submit(title, selftext = body)
+
 
 
 comment_thread = threading.Thread(target=comment_stream, args=(r, subreddit,))

58a6652ede358c2fc6306c96f912ca9b2446b5b6
NO ONE <38581702+HACKER097@users.noreply.github.com>
2021-11-04 18:44:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,24 +20,35 @@ def comment_stream(r, subreddit):
             parent = r.comment(comment.parent_id)
             comment.mod.remove()
 
-            if "!pass " in body:
-                body = body.split(" ")
+            if "||password||" in body:
+                body = body.split("||")
+
+                print(body)
 
                 for i in range(len(body)):
-
-                    if body[i] == "!pass":
-                        user_hash = hashlib.sha256(bytes(body[i+1] + str(comment.author) , "utf-8" )).hexdigest()
+                    if body[i] == "password":
+                        password = body[i+1]
+                        user_hash = hashlib.sha256(bytes(password + str(comment.author) , "utf-8" )).hexdigest()
 
                         body.pop(i)
                         body.pop(i)
-
-
-                        body = "*USERHASH: " + str(user_hash) + "*" + " \n\n " + " ".join(body)
-                        body = body[:10000]
 
                         break
 
+                body = "USERHASH:" + user_hash + "\n\n" +  " ".join(body)
+                body = body[:10000]
+
+            else:
+                user_hash = hashlib.sha256(bytes(str(comment.author) + str(random.random()), "utf-8" )).hexdigest()
+                body = "USERHASH:" + user_hash + "\n\n" + body
+
             parent.reply(body)
+            comment.author.message("YOUR (u/" + str(comment.author) + ") COMMENT HAS BEEN REMOVED AND POSTED BY u/Anon-Bot", body)
+
+
+
+#user_hash = hashlib.sha256(bytes(body[i+1].split("\n")[0] + str(comment.author) , "utf-8" )).hexdigest()
+
 
 def post_stream(r, subreddit):
     for post in subreddit.stream.submissions(skip_existing=True):
@@ -47,24 +58,35 @@ def post_stream(r, subreddit):
 
             post.mod.remove()
 
-            if "!pass " in body:
+            if "||password||" in body:
+                body = body.split("||")
 
-                body = body.split(" ")
+                print(body)
 
                 for i in range(len(body)):
-                    if body[i] == "!pass":
-                        user_hash = hashlib.sha256(bytes(body[i+1] + str(post.author) , "utf-8" )).hexdigest()
+                    if body[i] == "password":
+                        password = body[i+1]
+                        user_hash = hashlib.sha256(bytes(password + str(post.author) , "utf-8" )).hexdigest()
 
                         body.pop(i)
                         body.pop(i)
-
-
-                        body = "*USERHASH: " + str(user_hash) + "*" + " \n\n " + " ".join(body)
-                        body = body[:10000]
 
                         break
 
+                body = "USERHASH:" + user_hash + "\n\n" +  " ".join(body)
+                body = body[:10000]
+
+            else:
+                user_hash = hashlib.sha256(bytes(str(post.author) + str(random.random()) , "utf-8" )).hexdigest()
+                body = "USERHASH:" + user_hash + "\n\n" + body
+
             subreddit.submit(title, selftext = body)
+
+            reply = "Title:" + title + "\n\n" + "Body:" + body
+            reply = reply[:10000]
+            post.author.message("YOUR (u/" + str(post.author) + ") POST HAS BEEN REMOVED AND POSTED BY u/Anon-Bot", reply)
+
+
 
 
 
